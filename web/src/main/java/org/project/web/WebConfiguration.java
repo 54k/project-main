1da8cdc585a5f790469f98f3bffa095216a2abd1
Korotaev Boris <korotaev@multivi.ru>
2014-04-09 21:01:10 +0600

    hello angular

--- web/src/main/java/org/project/web/WebConfiguration.java
+++ web/src/main/java/org/project/web/WebConfiguration.java
@@ -4,11 +4,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = "org.project")
 @EnableAutoConfiguration
 @EnableAspectJAutoProxy
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration {
 }
