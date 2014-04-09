cc20377a834b81a7de774b57c1dc818babb5a53e
Korotaev Boris <korotaev@multivi.ru>
2014-04-09 17:38:07 +0600

    static resources

--- web/src/main/java/org/project/web/WebConfiguration.java
+++ web/src/main/java/org/project/web/WebConfiguration.java
@@ -1,15 +1,34 @@
 package org.project.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = "org.project")
 @EnableAutoConfiguration
 @EnableAspectJAutoProxy
 @EnableWebMvc
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        if (resourceLoader.getResource("classpath:/static/index.html").exists()) {
+            registry.addViewController("/").setViewName("forward:index.html");
+        }
+    }
 }
