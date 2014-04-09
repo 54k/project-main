dd4780f2f60d7d4dd7e64cde39955f3080264ea5
Korotaev Boris <korotaev@multivi.ru>
2014-04-09 16:47:54 +0600

    Dumb tests

--- web/src/test/java/org/project/web/test/EventRestControllerTest.java
+++ web/src/test/java/org/project/web/test/EventRestControllerTest.java
@@ -2,12 +2,14 @@ package org.project.web.test;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.project.persistence.PersistenceConfiguration;
 import org.project.persistence.domain.EventEntity;
+import org.project.persistence.service.EventRepositoryService;
 import org.project.web.WebConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -18,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,12 +29,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringApplicationConfiguration(classes = {WebConfiguration.class, PersistenceConfiguration.class})
 @WebAppConfiguration
 public class EventRestControllerTest extends Assert {
-
+    @Autowired
+    private EventRepositoryService eventRepositoryService;
     @Autowired
     private WebApplicationContext context;
     private MockMvc mvc;
     private ObjectMapper mapper;
     private EventEntity eventEntity;
+    private Long idForSearch;
 
     @Before
     public void setup() {
@@ -44,14 +49,32 @@ public class EventRestControllerTest extends Assert {
         eventEntity.setDescription("description");
     }
 
-    @Test
-    public void testCreateEvent() throws Exception {
-        String content = mapper.writer().writeValueAsString(eventEntity);
-        mvc.perform(post("/events").contentType(MediaType.APPLICATION_JSON).content(content)).andExpect(status().isCreated());
+    @Before
+    public void fillData() {
+        EventEntity entity = new EventEntity();
+        entity.setTitle("title");
+        entity.setDescription("description");
+        entity = eventRepositoryService.getEventRepository().save(entity);
+        idForSearch = entity.getId();
+    }
+
+    @After
+    public void dropData() {
+        eventRepositoryService.getEventRepository().deleteAll();
     }
 
     @Test
-    public void testNotFoundEvent() throws Exception {
-        mvc.perform(get("/events/{id}", Long.MAX_VALUE)).andExpect(status().isNotFound());
+    public void testCreateEvent() throws Exception {
+        mvc.perform(post("/events").contentType(MediaType.APPLICATION_JSON).content(mapper.writer().writeValueAsString(eventEntity))).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testFindEventById() throws Exception {
+        mvc.perform(get("/events/{id}", idForSearch)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteEvent() throws Exception {
+        mvc.perform(delete("/events/{id}", idForSearch)).andExpect(status().isOk());
     }
 }
